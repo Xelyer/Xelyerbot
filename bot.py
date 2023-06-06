213c5d0a5d12315891dd2a2c2268bb267514d574
Nivins <nivinsvysakh2020@outlook.com>
2023-06-06 22:21:37 +0530

    added Github Searching !

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import requests
 import json
 import dotenv
 import os
+import aiohttp
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -18,9 +19,11 @@ bot = commands.Bot(command_prefix=prefix,intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"Bot is ready! Logged in as {bot.user.name} ({bot.user.id})")
-    await bot.change_presence(activity=discord.Game('!joke'))
+    print(f"Bot is ready! Logged in as {bot.user.name}")
+    print(f"Bot Status Is Spiced Up ! {bot.user.name}")
+    await bot.change_presence(activity=discord.Game('!joke and !search_repo <query>'))
 
+# Joke api 
 
 @bot.command()
 async def joke(ctx: commands.Context):
@@ -40,6 +43,23 @@ async def joke(ctx: commands.Context):
     await joke_msg.edit(embed=new_embed)
     await joke_msg.add_reaction('😂')
     await joke_msg.add_reaction('👎')
+
+# Github Search 
+
+@bot.command()
+async def search_repo(ctx, query):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f'https://api.github.com/search/repositories?q={query}') as response:
+            data = await response.json()
+            if 'items' in data:
+                for item in data['items']:
+                    name = item['name']
+                    url = item['html_url']
+                    description = item['description']
+                    await ctx.send(f'Name: {name}\nURL: {url}\nDescription: {description}')
+                    break
+            else:
+                await ctx.send('No repositories found.')
 
 
 
